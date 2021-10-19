34a0e4bd977b56ce2614829c642cd058cf89842a
Skillvendor <skillvendor193@gmail.com>
2021-10-19 20:15:52 +0300

    started implementing the design

--- src/App.js
+++ src/App.js
@@ -2,34 +2,26 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  // Link
 } from "react-router-dom";
+import styled from 'styled-components';
+
 import './App.css';
-import Citizenship from './components/citizenship';
+// import Citizenship from './components/citizenship';
 import Calendar from './components/calendar';
+import NewsPage from './pages/newsPage';
 
 function App() {
   return (
     <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/calendar">Calendar</Link>
-          </li>
-        </ul>
-
-        <Switch>
-          <Route path="/calendar">
-            <Calendar />
-          </Route>
-          <Route path="/">
-            <Citizenship />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/calendar">
+          <Calendar />
+        </Route>
+        <Route path="/">
+          <NewsPage />
+        </Route>
+      </Switch>
     </Router>
   );
 }
---
+++ src/newsIssues/index.js
@@ -0,0 +1,8 @@
+
+import Oct2021Issues from './2021-oct';
+
+const allIssues = [
+ ...Oct2021Issues
+]
+
+export default allIssues
